52fa726b020dc44bda6a86adaa8788b798911d76
michael tu <michael.tu@yale.edu>
2022-04-12 02:10:05 -0400

    added tester and classical imp

--- classical_reflecting.py
+++ classical_reflecting.py
@@ -2,8 +2,10 @@
 import numpy as np 
 # matrix exponent, and more
 from scipy.linalg import expm
+# to graph things
+from matplotlib import pyplot as plt
 
-def classical_reflecting(n, drift, diffusion, t, initial):
+def classical_reflecting(n, drift, diffusion, t, target):
     #create hamiltonian
     h_dimension = n
     
@@ -14,7 +16,7 @@ def classical_reflecting(n, drift, diffusion, t, initial):
         a[0,i] = drift - (h_dimension / 2) + i
     
     #diffusion diagonals
-    b = np.zeros(1, h_dimension - 1)
+    b = np.zeros((1, h_dimension - 1))
     for i in range(0, h_dimension - 1):
         b[0,i] = diffusion
     
@@ -24,26 +26,33 @@ def classical_reflecting(n, drift, diffusion, t, initial):
     # compute the unitary
     U = expm(-(1j)*H)
 
-    # target state matrices
-    measure_A = np.zeros((h_dimension, h_dimension))
-    for i in range(h_dimension/2):
-        M = np.zeros((h_dimension, h_dimension))
-        M[i,i] = 1
-        measure_A += M
+    # measurement matrix for target state
+    measure = np.zeros((h_dimension, h_dimension))
+    measure[target, target] = 1
 
-    measure_B = np.zeros((h_dimension, h_dimension))
-    for i in range(h_dimension/2, h_dimension):
-        M = np.zeros((h_dimension, h_dimension))
-        M[i,i] = 1
-        measure_B += M
+    # initial, evenly distributed state vector (for 4 states only)
+    initial = [1/2, 1/2, 1/2, 1/2]
 
     # calculate each prob according to the formula
-    probA = abs(np.linalg.norm(measure_A @ np.linalg.matrix_power(U, t) @ initial))
-    probB = abs(np.linalg.norm(measure_B @ np.linalg.matrix_power(U, t) @ initial))
+    prob = abs(np.linalg.norm(measure @ np.linalg.matrix_power(U, t) @ initial))
 
-    print(probA, probB)
+    print("Probability of state " + str(target) + ": " + str(prob))
 
-    return [probA, probB]
+    return prob
 
+def classical_sim(n, drift, diffusion, t):
+    #setup
+    for t in range (0,t):
+        prob_list = []
+        for i in range(0,n):
+            prob_list.append(classical_reflecting(n, drift, diffusion, t, i))
+        print(prob_list)
+        states_list = ["|00>", "|01>", "|10>", "|11>"]
+        bar_plot = plt.bar(states_list, prob_list)
 
-
+        # display, see cs290 doc from Raghav for details
+        for bar in bar_plot:
+            yval = bar.get_height()
+            plt.text(bar.get_x()+0.2,yval+.01, round(yval, 3))
+        plt.savefig("./walk implementations/classical graphs/timestep=" + str(t), format='png')
+        plt.show()
